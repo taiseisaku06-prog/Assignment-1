09c2c2c3a7d5f1617f99c7a5e518502a3ce6481c
taiseisaku06-prog <taisei.saku06@gmail.com>
2026-09-20 15:37:22 +1000

    rubric 2 item was covered

---
+++ assembler.sh
@@ -0,0 +1,9 @@
+dir=($pwd)
+
+if [ $# -eq 0 ]; then 
+  echo -e "usage: no arg is provided.\n"
+  exit 1
+fi
+elif [ $# -gt 1]; then
+  echo -e "usage: more than one arguments are provided on STUDOUT.\n"
+fi
